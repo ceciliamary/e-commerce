65a34d5df39c61c846571a9685f87e0ac888dcd5
Cecilia Mary <ceciliamary78@gmail.com>
2024-03-13 16:11:55 -0300

    e-commerceDia1

---
+++ productos.js
@@ -0,0 +1,22 @@
+const Productos = [
+    { id: 1,
+    productoNombre: "Dulce de Frambuesa",
+    precio: 4500, 
+    cantidad: 1,
+    img: "./imagenes/Dulce de Frambuesas.jpg",
+    },
+
+    { id: 2,
+        productoNombre: "Dulce de Grosellas",
+        precio: 4800, 
+        cantidad: 1,
+        img: "./imagenes/Dulce de Grosellas.jpg",
+        },
+
+        { id: 3,
+            productoNombre: "Dulce de Rosa Mosqueta",
+            precio: 3500, 
+            cantidad: 1,
+            img: "./imagenes/Dulce de Rosa Mosqueta.jpg",
+            },
+];
